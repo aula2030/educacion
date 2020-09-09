9c547b6ceca45916a5171638105a393aa5c65325
AbdellahEO <abdellah.0297@gmail.com>
2020-09-09 23:28:01 +0200

    Activar el tema de Grav Learn2

---
+++ cache/compiled/files/c29721b1b7d0177c336a635146543052.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'C:/xampp/htdocs/educacion/user/config/media.yaml',
+    'modified' => 1599682648,
+    'data' => [
+        
+    ]
+];
