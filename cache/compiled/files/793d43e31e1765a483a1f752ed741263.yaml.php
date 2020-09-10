f182e470112217c55dfe585789f5c43670653223
AbdellahEO <abdellah.0297@gmail.com>
2020-09-10 13:18:46 +0200

    Eliminar ficheros innecesarios para correcta ejecucion de .gitignore

--- cache/compiled/files/793d43e31e1765a483a1f752ed741263.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'C:/xampp/htdocs/educacion/user/config/scheduler.yaml',
-    'modified' => 1599682648,
-    'data' => [
-        
-    ]
-];
